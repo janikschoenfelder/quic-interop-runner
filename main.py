e5e1ab178308de16e02b396ce4e831672c0dbd82
Janik Schönfelder <janik.schoenfelder@mailbox.tu-dresden.de>
2023-10-03 14:40:38 +0200

    Run in subprocess

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+from threading import Thread
 from typing import Union
+from uuid import uuid4
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from implementations import IMPLEMENTATIONS
 from interop import InteropRunner
 from pydantic import BaseModel
+from testcases import MEASUREMENTS
 
 app = FastAPI()
 
+job_status = {}
 
-class InteropConfig(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
+# class InteropConfig(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
 
 
 implementations = {
@@ -21,21 +25,51 @@ implementations = {
 }
 
 
+def run_interop(job_id):
+    try:
+        quic_measurement = None
+        for measurement in MEASUREMENTS:
+            if measurement.abbreviation() == "QP":
+                quic_measurement = measurement
+
+        InteropRunner(
+            implementations=implementations,
+            servers=["quiche"],
+            clients=["quiche"],
+            tests=[],
+            measurements=[quic_measurement],
+            output="output.json",
+            debug=True,
+            log_dir="",
+            save_files=False,
+        ).run()
+
+        job_status[job_id] = "completed"
+
+    except Exception as e:
+        # Optional: Fügen Sie Fehlerlogging hier hinzu
+        print(f"Error running InteropRunner: {str(e)}")
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/run")
-def run_interop():
-    return InteropRunner(
-        implementations=implementations,
-        servers=["quiche"],
-        clients=["quiche"],
-        tests=[],
-        measurements=["goodput"],
-        output="output.json",
-        debug=True,
-        log_dir="",
-        save_files=False,
-    ).run()
+@app.post("/start_interop")
+async def start_interop():
+    # Eindeutige Job-ID erstellen
+    job_id = str(uuid4())
+    job_status[job_id] = "running"
+    # InteropRunner in separatem Thread starten
+    Thread(target=run_interop, args=(job_id,)).start()
+    return {"status": "Interop runner started", "job_id": job_id}
+
+
+@app.get("/interop_status/{job_id}")
+async def get_interop_status(job_id: str):
+    # Jobstatus abrufen
+    status = job_status.get(job_id)
+    if status is None:
+        raise HTTPException(status_code=404, detail="Job not found")
+    return {"job_id": job_id, "status": status}
